74e5c365fd37aee941a945c7bc68e54acbd1c3f1
Tom Hutchinson <git@tomisme.com>
2016-06-23 09:42:25 +0800

    bump clojure, rename lein project

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject my-new-app "0.1.0-SNAPSHOT"
+(defproject re-starter "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
-                 [org.clojure/clojurescript "1.9.76"]
+                 [org.clojure/clojurescript "1.9.89"]
                  [devcards "0.2.1-7"]
                  [re-frame "0.7.0"]
                  [reagent "0.6.0-alpha"]
